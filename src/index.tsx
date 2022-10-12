d975ac41f4ace38e051399ea4ebbdc00db864ff7
Camila Sosa Morales <camilasosamorales@MacBook-Pro.fibertel.com.ar>
2022-10-12 16:07:28 -0300

    remove initial styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
 
 const store = createStore(reducer);
 
