6da469433c486ea36b5fd62287f5d1514ed1613a
Camila Sosa Morales <camilasosamorales@MacBook-Pro.fibertel.com.ar>
2022-10-12 16:26:29 -0300

    change import main

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "@mui/material/Container";
-import Main from "components/Main";
+import Main from "./components/Main";
 
 function App() {
   return (
