471a4be9e74561f5b752ef842b38b6e618cf116c
Camila Sosa Morales <camilasosamorales@MacBook-Pro.fibertel.com.ar>
2022-10-12 16:30:39 -0300

    fix body margin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './styles.scss';
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
